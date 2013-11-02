5d5033d3258a4e5e929f7bdadabd6462065fae40
Yong Guan <gyong9@gmail.com>
2013-11-02 03:07:06 -0700

    get the build working with ace shim

--- tasks/options/requirejs.js
+++ tasks/options/requirejs.js
@@ -3,15 +3,15 @@ module.exports = {
         options: {
             baseUrl: '<%= folder.src %>/js/',
             name: '<%= folder.src %>',
+            exclude: ['ace'],
             out: '<%= folder.dist %>/js/app.js',
             generateSourceMaps: false,
             optimize: 'uglify2',
             findNestedDependencies: true,
             inlineText: true,
-//            optimizeAllPluginResources: false,
             preserveLicenseComments: false,
             paths: {
-                ace: 'lib/ace/ace'
+                ace: './lib/ace/ace'
             },
             shim: {
                 'ace': {
